e5addda5466d35efb00e19f2ae6e3012a62130f2
jina555 <kja390@tukorea.ac.kr>
2025-09-18 18:16:56 +0900

    top line 완성

--- character_moves.py
+++ character_moves.py
@@ -3,21 +3,30 @@ from pico2d import *
 open_canvas()
 
 boy=load_image('character.png')
+grass=load_image('grass.png')
 
 
 def move_top():
+    print('Moving top')
+    for x in range(0,800,5):
+        draw(x,550)
+
+
     pass
 
 
 def move_right():
+    print('Moving right')
     pass
 
 
 def move_bottom():
+    print('Moving bottom')
     pass
 
 
 def move_left():
+    print('Moving left')
     pass
 
 
@@ -36,14 +45,19 @@ def move_circle():
     for deg in range(0,360):
         x= r * math.cos(math.radians(deg))+400
         y= r * math.sin(math.radians(deg))+300
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.1)
+        draw(x, y)
     pass
 
 
+def draw(x: float, y: float):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    boy.draw_now(x, y)
+    delay(0.1)
+
+
 while True:
-    move_circle()
+    # move_circle()
     move_rectangle()
 
     break
