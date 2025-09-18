998b0e4fab9e798d7d49a9f92462e7d3c0c4e0ee
jina555 <kja390@tukorea.ac.kr>
2025-09-18 17:44:33 +0900

    캐틱터 출력 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -2,6 +2,8 @@ from pico2d import *
 
 open_canvas()
 
+boy=load_image('character.png')
+
 
 def move_rectangle():
     print("Move rectangle")
@@ -10,12 +12,17 @@ def move_rectangle():
 
 def move_circle():
     print("Move circle")
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
 while True:
-    move_rectangle()
     move_circle()
+    move_rectangle()
+
+
     pass
 
 
